7293e1ffb2dce598f9695b38ecf90a539e0f06cc
bprasen1983 <prasenjit.b2@hcl.com>
2017-07-07 18:37:12 +0530

    Modifying travis yml file to push pack files to pact broker

--- Upload_pacts.sh
+++ Upload_pacts.sh
@@ -8,6 +8,7 @@ curl -X PUT \
   -H 'postman-token: daba02ed-2c00-befe-cdc5-4917366286c9' \
   -d @/home/travis/build/phil-rice/PactDemoMaster/target/pacts/CustomeIos_Provider.json
 
+
 curl -X PUT \
     https://hcl.pact.dius.com.au/pacts/provider/Provider/consumer/CustomeAndroid/version/1.0.0 \
     -H 'authorization: Basic aGxIUGZjdzl4TGxLT2g2ZDMxWkZlTDM3dE14azFtVzpFZWlRQTN4b1RZM2ZFZmpmOTRPMzZ6YVdYc3JuYUda' \
