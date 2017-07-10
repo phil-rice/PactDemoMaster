30dbddab5b24d161da469b6cddb99eac2221eb9a
Aban Sikhar Mitra <abanmitra@gmail.com>
2017-07-10 13:03:54 +0530

    Modified File

--- Upload_pacts.sh
+++ Upload_pacts.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# #####################################
+#	Delete All Existing PACT files
+# #####################################
+curl -X DELETE \
+	https://hcl.pact.dius.com.au/pacts/provider/Provider/consumer/CustomeIos/version/1.0.0 \
+	-H 'authorization: Basic aGxIUGZjdzl4TGxLT2g2ZDMxWkZlTDM3dE14azFtVzpFZWlRQTN4b1RZM2ZFZmpmOTRPMzZ6YVdYc3JuYUda'
+
+curl -X DELETE \
+	https://hcl.pact.dius.com.au/pacts/provider/Provider/consumer/CustomeAndroid/version/1.0.0 \
+	-H 'authorization: Basic aGxIUGZjdzl4TGxLT2g2ZDMxWkZlTDM3dE14azFtVzpFZWlRQTN4b1RZM2ZFZmpmOTRPMzZ6YVdYc3JuYUda'
+
+
+	
+# #####################################
+#	Update PACT files
+# #####################################
+	
 curl -X PUT \
   https://hcl.pact.dius.com.au/pacts/provider/Provider/consumer/CustomeIos/version/1.0.0 \
   -H 'authorization: Basic aGxIUGZjdzl4TGxLT2g2ZDMxWkZlTDM3dE14azFtVzpFZWlRQTN4b1RZM2ZFZmpmOTRPMzZ6YVdYc3JuYUda' \
