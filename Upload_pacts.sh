f575b2e07fa3db0443c0cdd688b0ee40aac776ed
phil <phil.rice@iee.org>
2017-07-19 10:08:22 +0200

    removing comment

--- Upload_pacts.sh
+++ Upload_pacts.sh
@@ -20,12 +20,10 @@ echo  " Username - $PACT_BROKER_USERNAME  password - $PACT_BROKER_PASSWORD" | se
 curl -X DELETE \
 	https://hcl.pact.dius.com.au/pacts/provider/Provider/consumer/CustomeIos/version/1.0.0 \
 	-u $username:$password
-#	-H 'authorization: Basic aGxIUGZjdzl4TGxLT2g2ZDMxWkZlTDM3dE14azFtVzpFZWlRQTN4b1RZM2ZFZmpmOTRPMzZ6YVdYc3JuYUda'
 
 curl -X DELETE \
 	https://hcl.pact.dius.com.au/pacts/provider/Provider/consumer/CustomeAndroid/version/1.0.0 \
 	-u $username:$password
-#	-H 'authorization: Basic aGxIUGZjdzl4TGxLT2g2ZDMxWkZlTDM3dE14azFtVzpFZWlRQTN4b1RZM2ZFZmpmOTRPMzZ6YVdYc3JuYUda'
 
 
 	
@@ -37,14 +35,13 @@ curl -X PUT \
   https://hcl.pact.dius.com.au/pacts/provider/Provider/consumer/CustomeIos/version/1.0.0 \
   -H 'cache-control: no-cache' \
   -H 'content-type: application/json' \
-  -u $username:$password
+  -u $username:$password \
   -d @./target/pacts/CustomeIos_Provider.json
 
 
 curl -X PUT \
     https://hcl.pact.dius.com.au/pacts/provider/Provider/consumer/CustomeAndroid/version/1.0.0 \
-#    -H 'authorization: Basic aGxIUGZjdzl4TGxLT2g2ZDMxWkZlTDM3dE14azFtVzpFZWlRQTN4b1RZM2ZFZmpmOTRPMzZ6YVdYc3JuYUda' \
     -H 'cache-control: no-cache' \
     -H 'content-type: application/json' \
-    -u $username:$password
+    -u $username:$password \
     -d @./target/pacts/CustomeAndroid_Provider.json
