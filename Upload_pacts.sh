af192fb7b7263ea9e1a3b0397d51303cab74422f
phil <phil.rice@iee.org>
2017-07-19 10:03:47 +0200

    removing comment

--- Upload_pacts.sh
+++ Upload_pacts.sh
@@ -35,7 +35,6 @@ curl -X DELETE \
 	
 curl -X PUT \
   https://hcl.pact.dius.com.au/pacts/provider/Provider/consumer/CustomeIos/version/1.0.0 \
-#  -H 'authorization: Basic aGxIUGZjdzl4TGxLT2g2ZDMxWkZlTDM3dE14azFtVzpFZWlRQTN4b1RZM2ZFZmpmOTRPMzZ6YVdYc3JuYUda' \
   -H 'cache-control: no-cache' \
   -H 'content-type: application/json' \
   -u $username:$password
