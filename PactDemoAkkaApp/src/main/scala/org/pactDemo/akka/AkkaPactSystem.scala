7686604387a811f727eedb529caefac260b2871b
Aban Sikhar Mitra <abanmitra@gmail.com>
2017-07-20 21:16:39 +0530

    Modified and add REST call

--- PactDemoAkkaApp/src/main/scala/org/pactDemo/akka/AkkaPactSystem.scala
+++ PactDemoAkkaApp/src/main/scala/org/pactDemo/akka/AkkaPactSystem.scala
@@ -6,8 +6,10 @@ import java.util.Calendar
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
-import com.twitter.finatra.http
-import org.pactDemo.utilities.PactArrow
+import com.twitter.finagle.Http
+import com.twitter.finagle.http.{Method, Request, Response}
+
+import org.pactDemo.utilities.{CustomeRequestProcessor, CustomeResponseProcessor, GenericCustomClient, PactArrow}
 
 
 object Util {
@@ -18,81 +20,143 @@ case class ProcessRequest(input: String)
 
 case object closeProcess
 
-trait StringToObject[String, ToObj ] extends ( String => ToObj )
-case class CustomRequestObject(id: Int, token: String, requestType: String)
-object CustomRequestObject {
-  implicit object makeObject extends StringToObject[ String, CustomRequestObject] {
+trait StringToObject[String, T] extends (String => T)
+
+object StringToObject {
+
+  implicit object makeObject extends StringToObject[String, CustomRequestObject] {
     val mapper = new ObjectMapper()
     mapper.registerModule(DefaultScalaModule)
+
     override def apply(input: String): CustomRequestObject = {
       mapper.readValue(input, classOf[CustomRequestObject])
     }
   }
+
 }
 
-trait CoreRequest
 
-case class ValidRequestProcess(param: CustomRequestObject) extends CoreRequest
-case class InvalidRequestProcess(param: CustomRequestObject) extends CoreRequest
+case class CustomRequestObject(id: Int, token: String)
 
-class AkkaPactSystem extends Actor with PactArrow {
+object CustomRequestObject {
+
+  implicit object makeRequest extends CustomeRequestProcessor[CustomRequestObject] {
+    override def apply(custObject: CustomRequestObject): Request = {
+      print(s"\n\n id: ${custObject.id}, token: ${custObject.token} \n\n")
+      val request = Request(s"/token/id/${custObject.id}")
+      request.headerMap.add("ContentType", "application/hcl.token")
+      request.method = Method.Post
+      request.setContentString(s"""{"Authentication-token":"token ${custObject.token}"}""")
+      request
+    }
+  }
+
+}
+
+
+//"""{"token":"12345-valid-for-id-1-token","id":"1", "valid": true,"server":"android"}"""
+case class DummyObj(id: Int, token: String, valid: Boolean, server: String)
+
+object DummyObj {
+
+  implicit object makeObject extends StringToObject[String, DummyObj] {
+    val mapper = new ObjectMapper()
+    mapper.registerModule(DefaultScalaModule)
+
+    override def apply(input: String): DummyObj = {
+      mapper.readValue(input, classOf[DummyObj])
+    }
+  }
+
+}
+
+case class CustomReplyObject(id: Int, token: String, valid: Boolean, server: String)
+
+object CustomReplyObject {
+
+  implicit object makeCustomResponse extends CustomeResponseProcessor[CustomReplyObject] with PactArrow {
+    override def apply(response: Response): CustomReplyObject = {
+
+      println(s"\n response.contentString : ${response.contentString}\n")
+
+      val convert = implicitly[StringToObject[String, DummyObj]]
+      val dummyObj = response.contentString ~> convert
+
+      CustomReplyObject(dummyObj.id, dummyObj.token, dummyObj.valid, dummyObj.server)
+
+      /*response.contentString.contains("invalid") match {
+        case true => response.contentString ~> convert//CustomReplyObject(2,"54321-invalid-for-id-2-token",false)
+        case false => response.contentString ~> convert//CustomReplyObject(1,"12345-valid-for-id-1-token",true)
+      }*/
+    }
+  }
+
+}
+
+
+case class CustomRequest(param: CustomRequestObject)
+
+class AkkaPactSystem(restClient: GenericCustomClient[CustomRequestObject, CustomReplyObject]) extends Actor with PactArrow {
 
   override def receive: Receive = {
     case ProcessRequest(input: String) => {
 
       val convert = implicitly[StringToObject[String, CustomRequestObject]]
-      val inputInfo = input ~> convert
+      val inputRequest = input ~> convert
 
-      if(inputInfo.id == 1143) {
-        val validProcess: ActorRef = context.actorOf(Props[ValidProcessActor], s"${inputInfo.requestType}-Process")
-        validProcess ! ValidRequestProcess (inputInfo)
-      } else if(inputInfo.id == 9905) {
-        val invalidProcess: ActorRef = context.actorOf(Props[InvalidProcessActor], s"${inputInfo.requestType}-Process")
-        invalidProcess ! InvalidRequestProcess (inputInfo)
-      }
+      // val cusToReq = implicitly[CustomeRequestProcessor[CustomRequestObject]]
+      // val req = input ~> cusToReq
+      // val inputRequest = input ~> convert //~> customToRequest
+
+      val requestProcessor: ActorRef = context.actorOf(Props(new CustomRequestProcessActor(restClient)), s"${inputRequest.id}-Processor")
+      requestProcessor ! CustomRequest(inputRequest)
     }
+
     case closeProcess =>
       println("System is going to shutdown @ " + Util.currentTime)
       context.system.terminate
   }
 }
 
-class ValidProcessActor extends Actor {
-  override def receive: Receive = {
-    case ValidRequestProcess(inputInfo) =>
-      println("---> " + inputInfo.id + " ::: " + inputInfo.requestType + " ::: " + inputInfo.token + "\n\n")
+class CustomRequestProcessActor(restClient: GenericCustomClient[CustomRequestObject, CustomReplyObject]) extends Actor with PactArrow {
 
+  override def receive: Receive = {
+    case CustomRequest(request) =>
+
+      /*restClient(request).onSuccess(println(_))
+      val g = restClient(request).onFailure(println(_))
+      g*/
+
+      restClient(request)
+
+    //def client: CustomRequestObject => Future[CustomReplyObject]
+    //string - CustomRequestObject - Request - > Response - CustomReplyObject
 
     case _ => println("None - valid")
   }
 }
 
-class InvalidProcessActor extends Actor {
-  override def receive: Receive = {
-    case InvalidRequestProcess(inputInfo) =>
-      println("---> " + inputInfo.id + " ::: " + inputInfo.requestType + " ::: " + inputInfo.token + "\n\n")
-
-
-    case _ => println("None - invalid")
-  }
-}
-
 object AkkaPactSystem {
 
-  def akkaProcessing() = {
+  def akkaProcessing(host: String, port: String) = {
     val system = ActorSystem("AkkaPactSystem")
-    val mainProcess: ActorRef = system.actorOf(Props[AkkaPactSystem], "Main-Process")
 
-    mainProcess ! ProcessRequest ("""{"id": 1143, "token":"1143-valid-token", "requestType":"valid" }""")
-    mainProcess ! ProcessRequest ("""{"id": 9905, "token":"9905-invalid-token", "requestType":"invalid"}""")
+    val rawHttpClient = Http.newService(host + ":" + port)
+    val restClient = new GenericCustomClient[CustomRequestObject, CustomReplyObject](rawHttpClient)
+
+    val mainProcess: ActorRef = system.actorOf(Props(new AkkaPactSystem(restClient)), "Main-Process")
+
+    mainProcess ! ProcessRequest("""{"id": 1, "token":"12345-valid-for-id-1-token"}""")
+    mainProcess ! ProcessRequest("""{"id": 2, "token":"54321-invalid-for-id-2-token"}""")
 
     Thread.sleep(50)
     mainProcess ! closeProcess
   }
 
   def main(args: Array[String]): Unit = {
-    akkaProcessing
+    val host = "localhost"
+    val port = "9020"
+    akkaProcessing(host, port)
   }
-
 
 }
