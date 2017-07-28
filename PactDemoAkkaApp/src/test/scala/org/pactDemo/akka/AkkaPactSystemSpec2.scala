509d1d4b5f6b2178a5105f2875bb6d0edde44a87
Aban Sikhar Mitra <abanmitra@gmail.com>
2017-07-28 18:00:41 +0530

    Modify Code

--- PactDemoAkkaApp/src/test/scala/org/pactDemo/akka/AkkaPactSystemSpec2.scala
+++ PactDemoAkkaApp/src/test/scala/org/pactDemo/akka/AkkaPactSystemSpec2.scala
@@ -4,9 +4,11 @@ import akka.actor.{ActorSystem, Props}
 import akka.pattern.ask
 import akka.testkit.{ImplicitSender, TestKit}
 import akka.util.Timeout
+import com.twitter.util.Future
+import org.mockito.Mockito._
 import org.pactDemo.finatraUtilities.GenericCustomClient
 import org.pactDemo.utilities.PactDemoSpec2
-import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, Matchers, WordSpecLike}
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
@@ -16,25 +18,28 @@ class AkkaPactSystemSpec2 extends TestKit(ActorSystem("MySpec"))
   with ImplicitSender
   with WordSpecLike
   with BeforeAndAfterAll
+  with BeforeAndAfter
   with Matchers
   with PactDemoSpec2 {
 
   val pactClient = mock[GenericCustomClient[CustomRequestObject, CustomReplyObject]]
-  implicit val timeout = Timeout(5 seconds)
+  val pactActor = system.actorOf(Props(new AkkaPactSystem(pactClient)), "Main-Process")
+
+  implicit val timeout = Timeout(5 second)
 
   override def afterAll {
     TestKit.shutdownActorSystem(system)
   }
 
-  "An AkkaPactSystem using implicit sender " should {
+
+
+  "An AkkaPactSystem using implicit sender" should {
     "send back custom response" in {
+      when(pactClient.apply(CustomRequestObject(1, "validToken"))) thenReturn Future.value(CustomReplyObject(1, "validToken", true))
 
-      val pactActor = system.actorOf(Props(new AkkaPactSystem(pactClient)), "Main-Process")
-
-      val future = pactActor ? ProcessRequest("""{"id": 1, "token":"12345-valid-for-id-1-token"}""")
+      val future = pactActor ? ProcessRequest("""{"id": 1, "token":"validToken"}""")
       val result: String = Await.result(future, timeout.duration).asInstanceOf[String]
-
-      println(s"\n\n Return Result : ${result} \n\n")
+      println(s"\n\n output ::: ${result} \n\n")
 
     }
   }
